f93af643963e212770cc5a9849077d3efccd6899
rahul868 <raval.rahul868@gmail.com>
2016-08-02 09:46:59 +0530

    Add files via upload

---
+++ Models/StudentCoursesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Models
+{
+
+    [MustBeTrueAttribute(ErrorMessage = "Please Select Course")]
+    public class StudentCoursesViewModel
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
